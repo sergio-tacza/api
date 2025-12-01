6cf4e350024d649e792a6b208e3e16ac0f897eb1
sergio <sergiotacza14@gmail.com>
2025-12-01 23:10:30 +0100

    Proyecto Tacbarber Completo

--- src/main/resources/db/migration/V4__insert_admin.sql
+++ src/main/resources/db/migration/V4__insert_admin.sql
@@ -1,5 +1,4 @@
-INSERT INTO usuario (id, nombre, email, password, rol, activo)
-VALUES (100, 'admin', 'admin@tacbarber.com', '$2a$10$jQxrG42P6Wni9BqdGm1L2.NoFtm5tP.hxE5fqC6SJJwtCQ5eriJQ.', 'ADMIN', true);
+INSERT INTO usuario (id, nombre, apellidos, email, password_hash, rol)
+VALUES (100, 'Admin', 'TacBarber', 'admin@tacbarber.com', '$2a$10$jQxrG42P6Wni9BqdGm1L2.NoFtm5tP.hxE5fqC6SJJwtCQ5eriJQ', 'ADMIN');
 
 SELECT setval('usuario_seq', 100);
-
